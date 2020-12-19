14622fdb6fe46dd6c45b1ee1cb676ff87064070a
Furkan <36634372+Furkanf@users.noreply.github.com>
2020-12-19 23:02:42 +0300

    embedded işlemleri dosyaları

---
+++ JPA - embedded/Contact.java
@@ -0,0 +1,33 @@
+package com.proje.model;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Contact {
+    private String phoneNumber;
+    private String email;
+
+    public Contact() {
+    }
+
+    public Contact(String phoneNumber, String email) {
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
